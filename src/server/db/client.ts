3a6f4cdc19641f3441b710facb38e95cd8fb8fc2
Unknown <tyler.simoni.8@gmail.com>
2022-07-28 22:18:30 -0400

    Updates prisma client logging to add errors and warnings

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -8,8 +8,11 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["query"],
-  });
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  })
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
