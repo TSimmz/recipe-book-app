31196245b08d389e39dca1ab8b19f9225d440146
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-01-03 08:54:37 -0500

    Fixing build warnings and errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from 'next';
 import { trpc } from '@/utils/trpc';
 import { Metadata } from '@/components';
-import Image from 'next/image';
 import { Title, Button } from '@mantine/core';
 import { signIn } from 'next-auth/react';
-import styles from '../styles/Home.module.css';
 import Router from 'next/router';
 
 const Home: NextPage = () => {
@@ -21,9 +19,9 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <div>
       <Metadata title="Recipe Book App" />
-      <main className={styles.main}>
+      <main>
         <Title order={1}>Recipe Book App</Title>
         <Button
           onClick={handleSignInClick}
@@ -35,19 +33,6 @@ const Home: NextPage = () => {
           Sign In
         </Button>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
