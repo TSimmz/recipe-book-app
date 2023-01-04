a838783e6312e85e0c23044d15cad0cc1330f830
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-01-04 13:33:00 -0500

    Created tooltip component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as CreateRecipeBook } from './CreateRecipeBook';
 export { default as Layout } from './Layout';
 export { default as DashboardHeader } from './DashboardHeader';
 export { default as DashboardNavbar } from './DashboardNavbar';
+export { default as ArrowTooltip } from './ArrowTooltip';
