a20ae86f98feb5fcbc16e92a3219655f2ca28e90
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-01-24 10:50:13 -0500

    #40: Created initial Profile BE schema and sync

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,10 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
 
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../server/db/client";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '@/server/db/client';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -17,16 +17,15 @@ export const authOptions: NextAuthOptions = {
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    })
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    session: async ({session, user}) => {
-      session.id = user?.id
+    session: async ({ session, user }) => {
+      session.id = user?.id;
       return session;
-    } 
-  }
-  
+    },
+  },
 };
 
 export default NextAuth(authOptions);
