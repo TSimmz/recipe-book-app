4371e1880c2cddc2c1954067dccd69a846f342ad
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-01-24 23:41:16 -0500

    #42: Began header rework

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,26 +1,40 @@
 import {
   Header,
   Group,
-  Title,
+  TextInput,
   useMantineTheme,
   createStyles,
+  Avatar,
+  Code,
 } from '@mantine/core';
 import { AvatarMenu, CustomButton, CustomNavLink } from '@/components';
 import { useRouter } from 'next/router';
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
+import { IconSearch } from '@tabler/icons';
 
 const useStyles = createStyles((theme) => ({
-  headerTitle: {
+  header: {
+    background: theme.colors.dark[9],
+    borderColor: theme.colors.dark[9],
+    display: 'flex',
+    alignItems: 'center',
+  },
+  headerLogo: {
     cursor: 'pointer',
     fontSize: '32px',
     fontWeight: 'normal',
-    transition: 'all ease-in-out 150ms',
+    transition: 'all ease-in-out 200ms',
 
     '&:hover': {
-      scale: '1.02',
+      scale: '1.1',
       color: theme.colors.orange[3],
-      transition: 'all ease-in-out 150ms',
+      transition: 'all ease-in-out 200ms',
+    },
+  },
+  searchBar: {
+    '&:focus, &:active': {
+      outlineColor: theme.colors.orange[4],
     },
   },
 }));
@@ -43,43 +57,60 @@ const CustomHeader: React.FC<CustomHeaderProps> = ({
   };
 
   return (
-    <Header
-      height={60}
-      py={theme.spacing.sm}
-      px={theme.spacing.lg}
-      bg={theme.colors.dark[9]}
-      style={{ borderColor: theme.colors.dark[9] }}
-    >
-      <Group position="apart">
-        <Link href="/">
-          <Title className={classes.headerTitle}>Recipe Book</Title>
-        </Link>
-        {!session ? (
-          <CustomButton
-            label="Sign In"
-            active
-            onClickHandler={handleSignInClick}
+    <Header height={60} px={theme.spacing.lg} className={classes.header}>
+      <Group position="apart" w={'100%'}>
+        <Group>
+          <Link href="/">
+            <Avatar
+              className={classes.headerLogo}
+              alt="brand-logo"
+              color="orange"
+              radius="xl"
+              ff={theme.fontFamily}
+              h={40}
+              w={40}
+            >
+              RB
+            </Avatar>
+          </Link>
+          <TextInput
+            className={classes.searchBar}
+            aria-label="Search Recipe Book App"
+            placeholder="Search"
+            size={'md'}
+            radius="xl"
+            rightSection={<IconSearch size={12} stroke={1.5} />}
           />
-        ) : (
-          <Group position="right">
-            <CustomNavLink
-              label="Browse"
-              linkTo="/browse"
-              active={router.pathname === '/browse'}
+        </Group>
+
+        <Group position="right">
+          {!session ? (
+            <CustomButton
+              label="Sign In"
+              active
+              onClickHandler={handleSignInClick}
             />
-            <CustomNavLink
-              label="My Shelf"
-              linkTo="/my-shelf"
-              active={router.pathname === '/my-shelf'}
-            />
-            <CustomNavLink
-              label="My Pantry"
-              linkTo="/my-pantry"
-              active={router.pathname === '/my-pantry'}
-            />
-            <AvatarMenu menuPosition="bottom-end" />
-          </Group>
-        )}
+          ) : (
+            <>
+              <CustomNavLink
+                label="Browse"
+                linkTo="/browse"
+                active={router.pathname === '/browse'}
+              />
+              <CustomNavLink
+                label="My Shelf"
+                linkTo="/my-shelf"
+                active={router.pathname === '/my-shelf'}
+              />
+              <CustomNavLink
+                label="My Pantry"
+                linkTo="/my-pantry"
+                active={router.pathname === '/my-pantry'}
+              />
+              <AvatarMenu menuPosition="bottom-end" />
+            </>
+          )}
+        </Group>
       </Group>
     </Header>
   );
