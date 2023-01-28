e5215ac8a1f122ea8a82de746a7f8f65136d20f3
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-01-27 19:00:57 -0500

    Adds CSS reset selectors to global styles component

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,15 +1,73 @@
 import { Global, MantineTheme } from '@mantine/core';
 
-type GlobalStylesProps = {};
+interface IGlobalStyles {}
 
-const GlobalStyles: React.FC<GlobalStylesProps> = ({}: GlobalStylesProps) => {
+const GlobalStyles: React.FC<IGlobalStyles> = () => {
   return (
     <Global
-      styles={(theme: MantineTheme) => ({
-        '*': {
+      styles={(theme) => ({
+        /*
+          Uses Josh Comeau's CSS reset: https://www.joshwcomeau.com/css/custom-css-reset/
+        */
+
+        /*
+          1. Use a more-intuitive box-sizing model.
+        */
+        '*, *::before, *::after': {
           boxSizing: 'border-box',
-          fontFamily:
-            'Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+        },
+
+        /*
+          2. Remove default margin
+        */
+        '*': {
+          margin: 0,
+        },
+
+        /*
+          3. Allow percentage-based heights in the application
+        */
+        'html, body': {
+          height: '100%',
+        },
+
+        /*
+          Typographic tweaks!
+          4. Add accessible line-height
+          5. Improve text rendering
+        */
+        body: {
+          lineHeight: 1.5,
+          WebkitFontSmoothing: 'antialiased',
+        },
+
+        /*
+        6. Improve media defaults
+        */
+        'img, picture, video, canvas, svg': {
+          display: 'block',
+          maxWidth: '100%',
+        },
+
+        /*
+        7. Remove built-in form typography styles
+        */
+        'input, button, textarea, select': {
+          font: 'inherit',
+        },
+
+        /*
+        8. Avoid text overflows
+        */
+        'p, h1, h2, h3, h4, h5, h6': {
+          overflowWrap: 'break-word',
+        },
+
+        /*
+        9. Create a root stacking context
+        */
+        '#root, #__next ': {
+          isolation: 'isolate',
         },
       })}
     />
